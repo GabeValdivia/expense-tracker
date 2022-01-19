c7ae26ca00e99816c2d6963ded23d0474011af93
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-18 22:24:32 -0600

    Add function to remove transaction and update totals

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function addTransactionDOM(transaction) {
 	item.innerHTML = `
 		${transaction.text} <span>${sign}${Math.abs(
 		transaction.amount
-		)}</span> <button class="delete-btn">x</button>
+		)}</span> <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
 	`;
 
 	list.appendChild(item);
@@ -83,6 +83,13 @@ function updateValues() {
 	balance.innerText = `$${total}`;
 	money_plus.innerText = `$${income}`;
 	money_minus.innerText = `$${expense}`;
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+	transactions = transactions.filter(transaction => transaction.id !== id);
+
+	init();
 }
 
 // Init App
